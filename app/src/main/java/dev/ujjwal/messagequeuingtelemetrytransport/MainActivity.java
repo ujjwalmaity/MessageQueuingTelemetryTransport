15e465d29200872d3a81a6c1b114f6ce7a15cbca
ujjwal <ujjwalmaity98@gmail.com>
2019-11-04 14:10:29 +0530

    Connect with MQTT

--- app/src/main/java/dev/ujjwal/messagequeuingtelemetrytransport/MainActivity.java
+++ app/src/main/java/dev/ujjwal/messagequeuingtelemetrytransport/MainActivity.java
@@ -3,12 +3,51 @@ package dev.ujjwal.messagequeuingtelemetrytransport;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
 
 public class MainActivity extends AppCompatActivity {
+
+    String clientId;
+    MqttAndroidClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        clientId = MqttClient.generateClientId();
+        client = new MqttAndroidClient(this.getApplicationContext(), "tcp://broker.hivemq.com:1883", clientId);
+
+        findViewById(R.id.mqtt).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    IMqttToken token = client.connect();
+                    token.setActionCallback(new IMqttActionListener() {
+                        @Override
+                        public void onSuccess(IMqttToken asyncActionToken) {
+                            // We are connected
+                            Toast.makeText(MainActivity.this, "onSuccess", Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                            // Something went wrong e.g. connection timeout or firewall problems
+                            Toast.makeText(MainActivity.this, "onFailure", Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
